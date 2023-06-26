3af6d2fcb90f5b7a4b13d362942559426de88565
River Ferguson <riverferguson@gmail.com>
2023-06-26 14:16:01 -0700

    create app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+    const [boats, setBoats] = useState([])
+
+    useEffect(() => {
+        fetch("/boats")
+        .then((resp) => resp.json())
+        .then((boats) => setBoats(boats))
+    }, [])
+
+  return (
+    <main>
+        <BoatPage/>
+    </main>
+  )
 }
 
 export default App;
---
+++ client/src/components/BoatPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import React from 'react'
+
+
+const BoatPage = () => {
+  return (
+    <div>BoatCard</div>
+  )
+}
+
+export default BoatPage
