67a942ac09ce6342d4a5b6c1636f2a5614279df1
River Ferguson <riverferguson@gmail.com>
2023-06-26 11:02:56 -0700

    complete validations and begin seed.py

--- server/models.py
+++ server/models.py
@@ -44,7 +44,8 @@ class Owner(db.Model, SerializerMixin):
     __tablename__ = 'owners'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     bio = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
