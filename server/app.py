b1b065aaf1ef7d6ec21a7d7d7f6549f10da60b49
Drew <drwomble27@gmail.com>
2023-06-26 10:44:29 -0600

    i moved it back :(

--- server/app.py
+++ server/app.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-from models import db, Scientist, Mission, Planet
-from flask_restful import Api, Resource
-from flask_migrate import Migrate
-from flask import Flask, make_response, jsonify, request
-import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+# Standard library imports
 
+# Remote library imports
+from flask import request
+from flask_restful import Resource
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# Local imports
+from config import *
+from models import User, Recipe
 
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
+# Views go here!
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
