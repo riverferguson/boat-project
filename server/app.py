5532a7c15c0b4a5124a62771e0884698fe11e8d2
Drew <drwomble27@gmail.com>
2023-06-26 10:41:08 -0600

    moved config info into app

--- server/app.py
+++ server/app.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
+from models import db, Scientist, Mission, Planet
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request
+import os
 
-# Standard library imports
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
 
-# Local imports
-from config import app, db, api
-from models import User, Recipe
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-# Views go here!
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
